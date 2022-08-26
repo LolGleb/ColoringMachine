d0854a659cea19097c76d31aa6546ac9c507b375
Gleb Syomshchikov <g.semshchikov@corp.mail.ru>
2022-08-26 15:05:03 +0700

    Initial Commit

---
+++ ColoringMachine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColoringMachine
+//
+//  Created by g.semshchikov on 26.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

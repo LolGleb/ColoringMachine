f466ba6b5def940cc65819435119c2f9ce9f7c43
Gleb Syomshchikov <g.semshchikov@corp.mail.ru>
2022-08-26 16:05:22 +0700

    First commit

--- ColoringMachine/ViewController.swift
+++ ColoringMachine/ViewController.swift
@@ -9,11 +9,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var coloredView: UIView!
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    var redAmount: CGFloat = 0
+    var greenAmount: CGFloat = 0
+    var blueAmount: CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // MARK: Red Label
+        redLabel.text = "0"
+        
+        // MARK: Green Label
+        greenLabel.text = "0"
+        
+        // MARK: Blue Label
+        blueLabel.text = "0"
+        
+        // MARK: Red Slider
+        redSlider.value = 0
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 255
+        
+        // MARK: Green Slider
+        greenSlider.value = 0
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 255
+        
+        // MARK: Blue Slider
+        blueSlider.value = 0
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 255
     }
 
 
+    @IBAction func redSliderAction() {
+        redLabel.text = String(round(redSlider.value * 100) / 100.0)
+        coloredView.backgroundColor = UIColor(red: CGFloat(redSlider.value)/255, green: greenAmount/255, blue: blueAmount/255, alpha: 1)
+        redAmount = CGFloat(redSlider.value)
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenLabel.text = String(round(greenSlider.value * 100) / 100.0)
+        coloredView.backgroundColor = UIColor(red: redAmount/255, green: CGFloat(greenSlider.value)/255, blue: blueAmount/255, alpha: 1)
+        greenAmount = CGFloat(greenSlider.value)
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueLabel.text = String(round(blueSlider.value * 100) / 100.0)
+        coloredView.backgroundColor = UIColor(red: redAmount/255, green: greenAmount/255, blue: CGFloat(blueSlider.value)/255, alpha: 1)
+        blueAmount = CGFloat(blueSlider.value)
+    }
 }
 
